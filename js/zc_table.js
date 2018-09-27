26ede9cd2b17428145114dd0e56977d541c7dd1a
zcreturn0 <237350543@qq.com>
2018-09-27 15:30:50 +0800

    add 'zc_border',add pager if pageNum <= 7

--- js/zc_table.js
+++ js/zc_table.js
@@ -14,7 +14,7 @@ function ZC_Tools(){
 
 /**
  * 
- * if el had class 'className'
+ * @description if el had class 'className'
  * @param el (object)
  * @param className (string)
  * @returns (boolean)
@@ -34,7 +34,7 @@ ZC_Tools.prototype.hasClass = function(el,className){
 
 /**
  * 
- * el add class 'className'
+ * @description el add class 'className'
  * @param el (object)
  * @param className (string)
  * 
@@ -54,7 +54,7 @@ ZC_Tools.prototype.addClass = function (el,className){
 }
 
 /**
- * judge a number is an Integer and over 0
+ * @description judge a number is an Integer and over 0
  * @param num(any)
  * @returns boolean
  * 
@@ -73,11 +73,11 @@ ZC_Tools.prototype.judgeNumber = function(num){
 
 /**
  * 
- * init table with some necessary params
+ * @description init table with some necessary params
  * @param table_setting (object)
- * @el table to mount(object)
- * @caption text shown on the table caption(Array)
- * @field attr on data list in table_data(Array)
+ *      --el table to mount(object)
+ *      --caption text shown on the table caption(Array)
+ *      --field attr on data list in table_data(Array)
  * 
  */
 function ZC_Table(table_setting) {
@@ -96,14 +96,14 @@ function ZC_Table(table_setting) {
 }
 
 /**
- * initialize style with setting attribute
+ * @description initialize style with setting attribute
  */
 ZC_Table.prototype.init = function(){
-    var zc_border = this.el.getAttribute('zc_border');
+    
 }
 
 /**
- * set data into table and make pagination
+ * @description set data into table and make pagination
  */
 ZC_Table.prototype.update = function (table_data) {
     this.el.innerHTML = '';
@@ -111,7 +111,7 @@ ZC_Table.prototype.update = function (table_data) {
 
     var tr = document.createElement('tr');
     for(let item of this.captain){
-        tr.innerHTML += `<th>${item}</th>`;
+        tr.innerHTML += `<th class="zc_th">${item}</th>`;
     }
 
     this.tools.addClass(tr,'zc_caption_tr');
@@ -121,17 +121,27 @@ ZC_Table.prototype.update = function (table_data) {
     for(let item of table_data.list){
         var tr = document.createElement('tr');
         for(let attr of this.field){
-            tr.innerHTML += `<td>${item[attr]}</td>`;
-        };
+            tr.innerHTML += `<td class="zc_td">${item[attr]}</td>`;
+        }
         this.table.appendChild(tr);
     }
     this.el.appendChild(this.table);
+
+    var zc_border = this.el.getAttribute('zc_border');
+    if (zc_border != null) {
+        for (var item of document.querySelectorAll('.zc_th')){
+            this.tools.addClass(item, 'zc_border');
+        }
+        for (var item of document.querySelectorAll('.zc_td')) {
+            this.tools.addClass(item, 'zc_border');
+        }
+    }
 
     this.pagination(table_data.page,table_data.pageSize,table_data.total);
 }
 
 /**
- * pagination
+ * @description pagination
  */
 ZC_Table.prototype.pagination = function (page,pageSize,total){
     if (!this.tools.judgeNumber(page) || !this.tools.judgeNumber(pageSize)){
@@ -147,4 +157,27 @@ ZC_Table.prototype.pagination = function (page,pageSize,total){
         throw 'out of pageNum';
     }
 
+    if(pageNum <= 7){
+        var pager = document.createElement('div');
+        pager.innerHTML = '';
+        pager.className = 'pager';
+        var pre = document.createElement('div');
+        pre.className = 'pre pager-node';
+        pre.innerText = '<';
+        pager.appendChild(pre);
+        for(var i=1;i<=pageNum;i++){
+            var pgNode = document.createElement('div');
+            pgNode.className = 'pager-node';
+            pgNode.innerText = i;
+            if(i == page){
+                this.tools.addClass(pgNode,'pager-selected');
+            }
+            pager.appendChild(pgNode);
+        }
+        var next = document.createElement('div');
+        next.className = 'next pager-node';
+        next.innerText = '>';
+        pager.appendChild(next);
+        this.el.appendChild(pager);
+    }
 }
